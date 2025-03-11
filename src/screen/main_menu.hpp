0d93859b645de69f975b9d3eea4b951846ddc8ca
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-03-12 02:30:44 +0700

    feat(screen/main_menu): add buttons and a logo

--- src/screen/main_menu.hpp
+++ src/screen/main_menu.hpp
@@ -1,64 +1,80 @@
+#include <functional>
 #include <string>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include "../element/fps_hud.hpp"
 #include "screen.hpp"
+#include <src/element/button.hpp>
 
 class MainMenu : public Screen
 {
     SDL_Texture *bg_texture;
-    SDL_Texture *text;
-    SDL_Rect text_rect;
-    TTF_Font *font_menu;
-    SDL_Surface *surface_text;
-    SDL_Surface *surface_bg;
     SDL_Rect bg_rect;
     SDL_Renderer *renderer;
     SDL_Window *window;
     SDL_Event event;
-    FPSHUD *fps_hud;
+    Button *start_button;
+    Button *load_button;
+    Button *settings_button;
+    // Logo part
+    SDL_Texture *logo_texture;
+    SDL_Rect logo_rect;
+    uint64_t last_tick;
+    int last_frame;
 
 public:
     MainMenu(SDL_Renderer *renderer, SDL_Window *window)
     {
         this->renderer = renderer;
         this->window = window;
-        bg_texture = IMG_LoadTexture(renderer, "assets/images/bg.jpg");
-        font_menu = TTF_OpenFont("assets/fonts/Shizuru-Regular.ttf", 64);
-        surface_text = TTF_RenderUTF8_Blended(font_menu, "2048 pho riu", WHITE_RGB);
-        text = SDL_CreateTextureFromSurface(renderer, surface_text);
-        TTF_SizeUTF8(font_menu, "2048 pho riu", &surface_text->w, &surface_text->h);
-        text_rect.x = 36;
-        text_rect.w = surface_text->w;
-        text_rect.h = surface_text->h;
-        fps_hud = new FPSHUD(renderer, window);
+        SDL_LogVerbose(0, "Renderer: %p", renderer);
+        bg_texture = IMG_LoadTexture(renderer, "assets/img/bg.jpeg");
+        last_tick = SDL_GetTicks64();
+        last_frame = 240;
+        logo_texture = IMG_LoadTexture(renderer, "assets/img/logo.png");
+        logo_rect.x = 1280 / 2 - 512 / 2;
+        logo_rect.y = -76;
+        logo_rect.w = 512;
+        logo_rect.h = 512;
+        // Buttons
+        start_button = new Button(renderer, window, "Start", 1280 / 2 - 300 / 2, 720 / 2, 300, 60, [&]()
+                                  { start_button_callback(); });
+        load_button = new Button(renderer, window, "Load", 1280 / 2 - 300 / 2, 720 / 2 + 80, 300, 60, [&]()
+                                     { settings_button_callback(); });
+        settings_button = new Button(renderer, window, "Settings", 1280 / 2 - 300 / 2, 720 / 2 + 80 + 80, 300, 60, [&]()
+                                     { settings_button_callback(); });
+    }
+    void start_button_callback()
+    {
+        SDL_LogVerbose(0, "Start button clicked.");
+    }
+    void settings_button_callback()
+    {
+        SDL_LogVerbose(0, "Settings button clicked.");
     }
     int handle_event(SDL_Event event)
     {
-        if (event.type == SDL_QUIT)
-        {
-            return 1;
-        }
+        start_button->handle_event(event);
+        settings_button->handle_event(event);
+        load_button->handle_event(event);
         return 0;
     }
     void render()
     {
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, bg_texture, NULL, NULL);
-        SDL_GetWindowSize(window, NULL, &text_rect.y);
-        text_rect.y -= 110;
-        SDL_RenderCopy(renderer, text, NULL, &text_rect);
-        fps_hud->render();
+        SDL_RenderCopy(renderer, logo_texture, NULL, &logo_rect);
+        (*start_button).render();
+        (*load_button).render();
+        (*settings_button).render();
     }
     ~MainMenu()
     {
         SDL_DestroyTexture(bg_texture);
-        SDL_DestroyTexture(text);
-        SDL_FreeSurface(surface_text);
-        TTF_CloseFont(font_menu);
-        delete fps_hud;
+        SDL_DestroyTexture(logo_texture);
+        delete start_button;
+        delete settings_button;
     }
     void destroy()
     {
--- src/screen/screen.hpp
+++ src/screen/screen.hpp
@@ -5,4 +5,5 @@ public:
     virtual int handle_event(SDL_Event event) = 0;
     virtual void render() = 0;
     virtual void destroy() = 0;
+    virtual ~Screen() = default;
 };
