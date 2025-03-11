2985a7730987b5cd8fa1b8dcc0c2d87029f98143
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-03-12 02:31:25 +0700

    chore(build): shared build instead for smaller executable size

---
+++ scripts/copy_deps.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+import shutil
+from pathlib import Path
+
+source_path = Path(os.getenv("MESON_SOURCE_ROOT"))
+build_path = Path(os.getenv("MESON_BUILD_ROOT"))
+install_path = build_path.parent / "bin"
+# Copy all dlls to the bin directory
+for dll in build_path.glob("**/*.dll"):
+    print(f"Copying {dll} to {install_path / 'bin' / dll.name}")
+    shutil.copy(dll, install_path / dll.name)
+for so in build_path.glob("**/*.so"):
+    print(f"Copying {so} to {install_path / 'lib' / so.name}")
+    shutil.copy(so, install_path / 'lib' / so.name)
+for dylib in build_path.glob("**/*.dylib"):
+    print(f"Copying {dylib} to {install_path / 'lib' / dylib.name}")
+    shutil.copy(dylib, install_path / 'lib' / dylib.name)
+# Copy assets
+print(f"Copying assets to {install_path / 'assets'}")
+shutil.copytree(source_path / 'assets', install_path / 'assets')
+print("Done!")
