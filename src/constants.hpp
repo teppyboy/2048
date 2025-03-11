b00af2ef2ffd21d8c6bc858db869ad51c91bdf34
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-03-12 02:29:22 +0700

    chore: remove assets in constants
    Because we already used the assets.hpp instead.

--- src/constants.hpp
+++ src/constants.hpp
@@ -8,8 +8,6 @@
 #define WINDOW_NAME "2048"
 
 SDL_Color WHITE_RGB = {255, 255, 255, 0};
-TTF_Font *DEBUG_FONT = nullptr;
-TTF_Font *UI_FONT = nullptr;
 enum class State
 {
     MAIN_MENU,
@@ -17,9 +15,3 @@ enum class State
     GAME_OVER,
     PAUSE
 };
-
-void init_statics()
-{
-    DEBUG_FONT = TTF_OpenFont("assets/fonts/jetbrains-mono.ttf", 12);
-    UI_FONT = TTF_OpenFont("assets/fonts/jetbrains-mono.ttf", 12);
-}
